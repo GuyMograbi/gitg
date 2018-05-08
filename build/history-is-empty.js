9df68531be9fc8450550ffebf6c36dd49224452a
Guy Mograbi <guy.mograbi@gmail.com>
2018-05-08 13:54:28 -0700

    first complete commit

---
+++ build/history-is-empty.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+if (fs.readFileSync(path.join(__dirname, '..','recent','history.txt')).toString().trim().length > 0){
+  console.error('history file should be empty');
+  process.exit(1);
+}
--- gitt.js
+++ gitt.js
@@ -1,21 +1,98 @@
 #!/usr/bin/env node
 const gitBranches = require('git-branch-away');
+var shell = require('shelljs');
 const path = require('path');
+var inquirer = require('inquirer');
+inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
 var argv = require('minimist')(process.argv.slice(2));
 const recentFilename = path.join(__dirname, 'recent','history.txt');
 const fs = require('fs');
+
+function getRecentlyUsed(){
+  return fs.readFileSync(recentFilename).toString().split('\n');
+}
+
 function printRecentlyUsed(){
-  console.log(fs.readFileSync(recentFilename).toString());
+  console.log(getRecentlyUsed().join('\n'));
+}
+
+function checkout(branch){
+  updateRecentlyUsed(branch);
+  shell.exec(`git checkout ${branch}`);
+}
+
+function updateRecentlyUsed(branch){
+  let recent = getRecentlyUsed();
+  recent = [branch, ...recent.filter(r=>r!== branch)];
+  fs.writeFileSync(recentFilename, recent.join('\n'));
+}
+
+if (argv.help){
+  console.log(
+    `
+      print current branch
+
+          gitt .
+
+      show recent branches used. (simply gitt without any parameter)
+
+         gitt
+
+      show all local branches (just like git branch)
+
+         gitt -
+
+       checkout while fuzzy searching (will fall back to interactive search if multiple options)
+
+          gitt some_name
+
+      checkout with interatice search (find)
+
+         gitt -f
+    `
+  )
+  process.exit(0);
+}
+
+function find(branches = gitBranches.list().all){
+
+  inquirer.prompt([
+    {
+    type: 'autocomplete',
+    name: 'branch',
+    message: 'start typing to search your branch',
+    // source: (answers, input) => ['mouse', 'house']
+    source: (answers, input) => Promise.resolve(input ? branches.filter((b) => b.includes(input)) : branches)
+  },
+]).then(answers=>{
+  checkout(answers.branch);
+});
 }
 
 // console.log('args are', argv);
 [branch] = argv._;
 // console.log('branch is', branch);
 //
-if (!branch){
+if (!branch && !argv.f){
   printRecentlyUsed();
 } else if (branch === '-') {
-  console.log(gitBranches.all().join('\n'));
+  console.log(gitBranches.list().all.join('\n'));
+} else if (branch === '.') {
+  console.log(gitBranches.list().current);
+} else if (branch) {
+  const matches = gitBranches.list().all.filter(b=>b.indexOf(branch) >= 0);
+  console.log('matches are', matches);
+  if (matches.length === 1){
+    checkout(matches[0]);
+  } else if (matches.length === 0){
+    console.log('no matches found. lets help you find it');
+    find();
+  } else {
+    console.log('multiple options found.');
+    find(matches);
+  }
+} else if (argv.f) {
+  find();
 }
 if (argv._[0])
 gitBranches.list();
